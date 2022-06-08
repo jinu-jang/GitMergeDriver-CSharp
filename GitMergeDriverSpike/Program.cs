8532012290aa869772253ac5d06007fad6724818
Jinu Jang <jimjang95@gmail.com>
2022-06-07 18:12:17 -0700

    Use git for original merge

--- GitMergeDriverSpike/Program.cs
+++ GitMergeDriverSpike/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Specialized;
+using System.Diagnostics;
 
 namespace GitMergeDriverSpike
 {
@@ -17,9 +18,38 @@ namespace GitMergeDriverSpike
             Console.WriteLine($"L: {inputArgs.ConflictMarkerSize}");
             Console.WriteLine($"P: {inputArgs.OutputFileName}");
 
-            File.ReadAllLines(inputArgs.AFileName);
+            GitMerge(inputArgs);
+
+            // Figure out branch names.
+            // Potential: MERGE_MSG in .git
+            // .git can be found through command `git rev-parse --git-dir`
+            // Potentially broken w/ configuration: http://git-scm.com/docs/git-merge#Documentation/git-merge.txt-mergesuppressDest
 
             Console.WriteLine($"Full array: [{string.Join(", ", args)}]");
+        }
+
+        private static string GitMerge(CmdInputArgs args)
+        {
+            Process p = new Process();
+
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.FileName = "git";
+            p.StartInfo.Arguments = $"merge-file --stdout {args.AFileName} {args.OFileName} {args.BFileName}";
+            p.Start();
+
+            string stdOut = p.StandardOutput.ReadToEnd();
+            string stdErr = p.StandardError.ReadToEnd();
+
+            p.WaitForExit();
+
+            if (stdErr.Length != 0)
+            {
+                throw new Exception(stdErr);
+            }
+            return stdOut;
         }
     }
 
